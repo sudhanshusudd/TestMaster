78a40acd76a37397b5eeeb2e2e2a2d9dced599e3
SudhanshuChaurasia <sudhanshu.chaurasia.sel@gmail.com>
2022-08-24 11:34:34 +0530

    Added testNg.xml

--- TestMaster/src/com/sudd/Init/Driver.java
+++ TestMaster/src/com/sudd/Init/Driver.java
@@ -1,8 +1,15 @@
 package com.sudd.Init;
 
+import org.testng.annotations.Test;
+
 public class Driver {
 	public static void main(String[] args) {
 		System.out.println("At driver class..");
+	}
+	
+	@Test
+	public void Run() {
+		System.out.println("Runner..");
 	}
 
 }
